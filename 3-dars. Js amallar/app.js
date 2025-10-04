43025c1112a3a66d2055cdc52946330d07fc3028
otabekkadirov <ganijanovich94@gmail.com>
2025-10-04 19:52:52 +0500

    3-dars: Amallar

---
+++ 3-dars. Js amallar/app.js
@@ -0,0 +1,72 @@
+// Amallar
+
+// String amallari:
+
+// "Salom " + "Buxoro"; // "Salom Buxoro"
+
+// let ism = "Akbar";
+// let familya = "Izzatov";
+
+// console.log("Salom " + ism + " " + familya); // String Concatenation
+// console.log(`Salom hammaga, men ${ism} ${familya}`); // String Interpolation
+
+// toUpperCase() - harflarni katta qiladi
+// let xabar = "bugun havo bulutli";
+// xabar = xabar.toUpperCase();
+
+// toLowerCase() - harflarni kichik qiladi
+// let sayyora = "venera";
+
+// console.log(xabar);
+// console.log(sayyora.toLowerCase());
+
+// length - stringni ichida nechta harf borligini ko'rsatadi
+// let ism = "! Murod";
+
+// "Murod"[0]; // "M"
+// "Murod"[1]; // "u"
+// "Murod"[2]; // "r"
+
+// console.log(ism[0]);
+// console.log(ism[1]);
+// console.log(ism[2]);
+
+// Matematik amallar
+// +
+
+// let a = 45;
+// let b = 65;
+
+// a = a + 100;
+
+// console.log(a + b);
+
+// console.log("10" + "20");
+// console.log("10" + 20);
+// console.log(7 + 8 + 5 + "1");
+// console.log("7x" + 5);
+// console.log("lyuboy string" + 12321);
+
+// -
+
+console.log(10 - 4);
+console.log("10" - 5);
+console.log(10 - 7);
+
+// *
+console.log("5" * 4);
+console.log(6 * "3");
+// /
+console.log("21t" / 3);
+
+// **
+console.log(2 ** 5); // 2 * 2 * 2 * 2 * 2
+console.log(5 ** 3); // 5 * 5 * 5
+console.log(10 ** 12);
+
+// % = qoldiq
+console.log(12 % 4);
+console.log(13 / 4);
+console.log(8 % 3); // 2
+console.log(8 / 3); //
+console.log("12g" % 5);
