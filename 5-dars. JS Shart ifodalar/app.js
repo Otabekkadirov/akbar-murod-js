8d7be004607199a1f17b6733cc0293a8a4f0cd3c
otabekkadirov <ganijanovich94@gmail.com>
2025-10-12 11:32:18 +0500

    5-dars. JS Shart ifodalari (if conditions)

---
+++ 5-dars. JS Shart ifodalar/app.js
@@ -0,0 +1,64 @@
+// Shart gaplar:
+
+// if (5 < 3) {
+//     console.log("salom");
+// }
+
+// let age = 13;
+// age = 21;
+
+// if (age > 18) {
+//     console.log("Saylovda ishtirok etish mumkin");
+// }
+
+// let age = 13;
+
+// age = 17;
+
+// if (age > 16) {
+//     console.log("Pasport olgan");
+// } else {
+//     console.log("Pasporti yo'q");
+// }
+
+// let admin = true;
+
+// if (admin) {
+//     console.log("Xush kelibsiz Admin");
+// } else {
+//     console.log("Xush kelibsiz mehmon");
+// }
+
+// let damOlish = true;
+// damOlish = false;
+
+// if (damOlish) {
+//     console.log("Bugun dars yo'q");
+// } else {
+//     console.log("bugun dars!");
+// }
+
+// let yosh = 11;
+
+// if (yosh > 16) {
+//     console.log("Pasporti bor");
+// } else if (yosh == 16) {
+//     console.log("Bor pasport ol");
+// } else if (yosh < 16) {
+//     console.log("hali pasport olmagan");
+// }
+
+// let fasl = "kuz";
+
+// let soat = 19.48;
+// soat = 20.15;
+
+// soat = 13.25;
+
+// if (soat >= 13 && soat <= 14) {
+//     console.log("Prounity obed qilyapti");
+// } else if (soat >= 9 && soat <= 20) {
+//     console.log("ProUnity Ochiq");
+// } else {
+//     console.log("ProUnity yopiq");
+// }
