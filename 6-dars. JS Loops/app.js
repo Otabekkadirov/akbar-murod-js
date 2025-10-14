b1728fb0458920773eadc7e99990490fd0ce503a
otabekkadirov <ganijanovich94@gmail.com>
2025-10-14 18:47:20 +0500

    6-dars. JS Loops while loop

---
+++ 6-dars. JS Loops/app.js
@@ -0,0 +1,24 @@
+// Loop
+
+// while
+
+// let son = 2;
+
+// while (son <= 40) {
+//     console.log("son: " + son);
+//     son = son + 2;
+// }
+
+let son = 100;
+
+while (son > 0) {
+    if (son == 70) {
+        console.log("ahahahah");
+    } else {
+        console.log(son);
+    }
+
+    son = son - 10;
+}
+
+console.log("halqa tugadi, men boshqa kodman");
