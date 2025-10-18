c4b985e0bfb81f006059846dec5201348293f086
otabekkadirov <ganijanovich94@gmail.com>
2025-10-18 18:49:49 +0500

    7-dars: Loops (for loop)

---
+++ 7-dars. JS Loops/app.js
@@ -0,0 +1,63 @@
+// Loop
+
+// while
+
+// let x = 10;
+
+// while (x > 0) {
+//     console.log(x);
+//     x = x - 1;
+// }
+
+// let x = 1;
+
+// while (true) {
+//     if (x == 22) {
+//         break;
+//     }
+
+//     console.log(x);
+//     x = x + 1;
+// }
+
+// let x = 0;
+
+// while (x < 100) {
+
+//     if (x == 40) {
+//         continue;
+//     }
+
+//     x = x + 10;
+//     console.log(x);
+// }
+
+// let x = 5;
+
+// do {
+//     console.log(x);
+//     x += 1;
+// } while (x < 10);
+
+// let parol;
+
+// do {
+//     parol = prompt("Parol nima?");
+
+//     if (parol == "123") {
+//         console.log("Parol to'gri, xush kelibsiz");
+//     }
+// } while (parol != "123");
+
+// let x = 1;
+// while (x < 10) {
+//     console.log(x);
+//     x = x + 1;
+// }
+
+// let dollar = "";
+
+// for (let x = 1; x <= 10; x += 1) {
+//     dollar = dollar + "$";
+//     console.log(dollar);
+// }
