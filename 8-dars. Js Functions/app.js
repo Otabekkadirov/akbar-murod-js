f7882c8ae40738ef30fe926df2740b7feb05ab0b
otabekkadirov <ganijanovich94@gmail.com>
2025-10-21 18:15:41 +0500

    feat: 8-dars: Js functions

---
+++ 8-dars. Js Functions/app.js
@@ -0,0 +1,41 @@
+// functions
+
+// function akmal() {
+//     console.log("salom akmal");
+// }
+
+// akmal();
+
+// function sayHello(name = "warrior") {
+//     let text = "Hello, and good morning " + name;
+//     alert(text);
+// }
+
+// sayHello("Murod");
+// sayHello("Akbar");
+// sayHello(true);
+// sayHello(null);
+// sayHello("Jasur");
+
+// function masala(nimadir) {
+//     let natija = nimadir * nimadir;
+//     console.log(natija);
+// }
+
+// masala(6);
+// masala(71);
+// masala(true);
+// masala(0);
+// masala("murod");
+
+// function triple(son) {
+//     let natija = son * son * son;
+//     console.log(natija);
+//     return natija;
+// }
+
+// triple(8);
+// triple(11);
+// let text = triple();
+
+// console.log(text);
