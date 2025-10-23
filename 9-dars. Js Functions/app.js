2e76ba6f9e0dd43798628a7135547a0daef9817d
otabekkadirov <ganijanovich94@gmail.com>
2025-10-23 18:27:21 +0500

    9-dars: Js Functions

---
+++ 9-dars. Js Functions/app.js
@@ -0,0 +1,65 @@
+// function multiply(x, y) {
+//     return x * y;
+// }
+
+// let a = multiply(100, 30);
+
+// console.log(a);
+
+// loop
+
+// function sana(x) {
+//     let son = 1;
+//     while (son <= x) {
+//         console.log(son);
+//         son += 1;
+//     }
+// }
+
+// sana(99);
+
+// function sana(x) {
+//     while (x >= 1) {
+//         console.log(x);
+//         x -= 1;
+//     }
+// }
+// sana(4);
+
+// boshHarf("Akbar") => "A"
+// boshHarf("Murod") => "M"
+// boshHarf("Jasur") => "J"
+
+// function boshHarf(nom) {
+//     console.log(nom[nom.length - 1]);
+// }
+
+// boshHarf("Jasur");
+// boshHarf("Begzod");
+// boshHarf("Hasan");
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function divideByThree(x) {
+//     return x / 3;
+// }
+
+// let natija = divideByThree(add(45, 95));
+
+// console.log(natija);
+
+// count(a, b) => a ... b
+
+// count(2, 8) => 2345678
+// count(11, 15) => 1112131415
+// count(1, 5) => 12345
+// count(1, 1) => 1
+// count(7, 10) => 78910
+// count(7, 1) => 7
+
+function count(a, b) {
+    // hich narsa yo'q
+}
+count(1, 3);
